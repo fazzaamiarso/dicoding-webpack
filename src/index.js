8b832fc0e47fc1a8ec56b6d801748faa2ca09edf
fazzaamiarso <fazzaamiarso@gmail.com>
2022-11-07 02:42:11 +0700

    setup more webpack config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello From Webpack!");
+import "./style.css";
+import "regenerator-runtime/runtime.js";
+
+const API_ENDPOINT = "https://the-trivia-api.com/api/questions?limit=5";
+const getQuestions = async () => {
+  const res = await fetch(API_ENDPOINT, { method: "GET" });
+  if (!res.ok) alert("Fetch Failed!");
+  return await res.json();
+};
+
+getQuestions().then(console.log);
---
+++ webpack.dev.js
@@ -0,0 +1,4 @@
+const { merge } = require("webpack-merge");
+const webpackCommon = require("./webpack.common");
+
+module.exports = merge(webpackCommon, { mode: "development" });
